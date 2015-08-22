9f2b15208fcffd94f4b8557ce6d36f9057813d3d
Nicholas Pappagalo <npappagalo@gmail.com>
2015-08-22 14:53:22 -0700

    Add component validation with proptypes

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,10 +1,14 @@
 var React = require('react');
 
 var Repos = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    repos: React.PropTypes.array.isRequired
+  },
   render: function() {
     return (
       <div>
-        Repos: {this.props.repos} 
+        Repos: {this.props.repos}
       </div>
     );
   }
--- public/bundle.js
+++ public/bundle.js
@@ -23679,6 +23679,10 @@
 	var Repos = React.createClass({
 	  displayName: 'Repos',
 
+	  propTypes: {
+	    username: React.PropTypes.string.isRequired,
+	    repos: React.PropTypes.array.isRequired
+	  },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
@@ -23702,6 +23706,10 @@
 	var UserProfiles = React.createClass({
 	  displayName: 'UserProfiles',
 
+	  propTypes: {
+	    username: React.PropTypes.string.isRequired,
+	    bio: React.PropTypes.object.isRequired
+	  },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
@@ -23731,6 +23739,10 @@
 	var Notes = React.createClass({
 	  displayName: 'Notes',
 
+	  propTypes: {
+	    username: React.PropTypes.string.isRequired,
+	    notes: React.PropTypes.array.isRequired
+	  },
 	  render: function render() {
 	    return React.createElement(
 	      'div',
